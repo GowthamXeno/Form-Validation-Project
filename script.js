d6e2652f9210b5ba02844519265dfac776dd7bd1
GowthamXeno <gowthamudpm2004@gmail.com>
2023-12-14 14:30:59 +0530

    Completed the Project

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function Form() {
+// JavaScript logic for form validation
+
+function Form(event) {
+// Prevent form submission
     event.preventDefault();
+
+// Get form field values
     var username = document.getElementById('username').value.trim();
     var email = document.getElementById('email').value.trim();
     var number = document.getElementById('number').value.trim();
@@ -8,7 +13,7 @@ function Form() {
     var confirmpassword = document.getElementById('confirmpassword').value.trim();
     var dob = document.getElementById('dob').value.trim();
 
-
+// Function to show error message
     function showerror(Id, message) {
 
         var error = document.getElementById(Id + 'Error');
@@ -19,6 +24,7 @@ function Form() {
 
     }
 
+// Function to hide error message
     function hideerror(Id) {
 
         var error = document.getElementById(Id + 'Error');
@@ -26,110 +32,121 @@ function Form() {
         error.textContent = '';
         error.style.display = 'none';
         error_container.style.display = 'none';
+
     }
     
-    //Username
-        if(username === ''){
+//Username Field
+        if(username === '')
             showerror('username', 'Please Enter Username!');
-        }else{
-            if (username.length < 5) {
+        else{
+
+            if (username.length < 5)
                 showerror('username', 'Name must be at least 5 characters');
-            } else {
+            else
                 hideerror('username');
-            }
         }
     
-    //Email
+//Email Field
         var condition = /@.*\./;
-        if(email ===''){
+
+        if(email ==='')
             showerror('email', 'Please Enter email!');
-        }
         else{
-            if (!email.includes('@')) {
+
+            if (!email.includes('@')) 
                 showerror('email', 'Email must contain \'@\'');
-            } 
-            else if(!condition.test(email)){
+            else if(!condition.test(email))
                 showerror('email', 'Enter a Valid Email');
-            }
-            else {
+            else 
                 hideerror('email');
-            }
+            
         } 
      
-    //Number
+//Number Field
         var numbercondition = /^\d{10}$/
+
         if(number === '')
             showerror('number', 'Please Enter Number!');
         else{
-            if(number.length < 10 || number === '123456789' || !numbercondition.test(number)){
+
+            if(number.length < 10 || number === '123456789' || !numbercondition.test(number))
                 showerror('number','Enter a Valid Number!');
-            }
-            else{
+            else
                 hideerror('number');
-            }
+            
         }
 
-    //Gender
+//Gender Field
         if(gender === '')
             showerror('gender', 'Please Select Gender!');
         else
             hideerror('gender');
     
-    //Dob
+//Dob Field
         if(dob === '')
             showerror('dob', 'Please Enter Date of Birth!');
-        else{
+        else
             hideerror('dob');
-        }
+        
     
-    //Password
+//Password Field
         if(password === '')
             showerror('password', 'Please Enter Password!');
+
         else{
+
             if(password.toLowerCase() === 'password'){
-                console.log(password.toLowerCase()+'!== password');
-                console.log(password.toLowerCase()+'!=='+username.toLowerCase());
-                showerror('password','Password cannot be ‘password’');
+                    console.log(password.toLowerCase()+'!== password');
+                    console.log(password.toLowerCase()+'!=='+username.toLowerCase());
+                    showerror('password','Password cannot be ‘password’');
             }
-            else if(password.toLowerCase() === username.toLowerCase()){
-                showerror('password','Password cannot be ‘name of the user’');
-            }
-            else if(password.length < 8){
-                showerror('password','Password must be at least 8 characters')
-            }
-            else{
+            else if(password.toLowerCase() === username.toLowerCase())
+                    showerror('password','Password cannot be ‘name of the user’');
+            else if(password.length < 8)
+                    showerror('password','Password must be at least 8 characters')
+            else
                     hideerror('password');
-                }
         }
     
-    //Confirmpassword
+//Confirmpassword Field
         if(confirmpassword === '')
             showerror('confirmpassword', 'Please Enter Confirm Password!');
         else{
-            if(password !== confirmpassword){
+
+            if(password !== confirmpassword)
                 showerror('confirmpassword','Password must match with the confirm password');
-            }
-            else{
+            else
             hideerror('confirmpassword');
-            }
         
         }
-    //submit
+
+ // Check overall form validity
         var isValid = true;
+
         document.querySelectorAll('.error_container').forEach(function (errorContainer) {
+
             if (errorContainer.style.display === 'flex') {
                 isValid = false;
-        }
-    });
+
+            }
+        });
+
+// Show success toast and clear form if valid
         if(isValid){
+
             var toast = document.getElementById('toast');
             toast.classList.add('show');
             clearForm();
+
             setTimeout(function () {
                 toast.classList.remove('show');
             }, 2000);
+
         }
+
+// Function to clear form fields
         function clearForm() {
+
             document.getElementById('username').value = '';
             document.getElementById('email').value = '';
             document.getElementById('number').value = '';
@@ -137,6 +154,7 @@ function Form() {
             document.getElementById('password').value = '';
             document.getElementById('confirmpassword').value = '';
             document.getElementById('dob').value = '';
+
         }
         
         
